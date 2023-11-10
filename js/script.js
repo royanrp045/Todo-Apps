f31d99422177dc5447ef4196444df049ebcb957e
Royanrp045 <royanristuprayoga@gmail.com>
2023-11-10 10:41:32 +0700

    add remove and undo

--- js/script.js
+++ js/script.js
@@ -119,3 +119,31 @@ document.addEventListener(RENDER_EVENT, function () {
     else completedTODOList.append(todoElement);
   }
 });
+
+function removeTaskFromCompleted(todoId) {
+  const todoTarget = findTodoIndex(todoId);
+
+  if (todoTarget === -1) return;
+
+  todos.splice(todoTarget, 1);
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function undoTaskFromCompleted(todoId) {
+  const todoTarget = findTodo(todoId);
+
+  if (todoTarget == null) return;
+
+  todoTarget.isCompleted = false;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function findTodoIndex(todoId) {
+  for (const index in todos) {
+    if (todos[index].id === todoId) {
+      return index;
+    }
+  }
+
+  return -1;
+}
