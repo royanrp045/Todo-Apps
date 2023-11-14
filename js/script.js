c8700298b94d02a0eb054eec0c307ced49779fd3
Royanrp045 <royanristuprayoga@gmail.com>
2023-11-14 20:39:11 +0700

    finish learn

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
     event.preventDefault();
     addTodo();
   });
+
+  if (isStorageExist()) {
+    loadDataFromStorage();
+  }
 });
 
 function addTodo() {
@@ -174,3 +178,16 @@ function isStorageExist() /* boolean */ {
 document.addEventListener(SAVED_EVENT, function () {
   console.log(localStorage.getItem(STORAGE_KEY));
 });
+
+function loadDataFromStorage() {
+  const serializedData = localStorage.getItem(STORAGE_KEY);
+  let data = JSON.parse(serializedData);
+
+  if (data !== null) {
+    for (const todo of data) {
+      todos.push(todo);
+    }
+  }
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
